30fb6183f2ecec128907ce815ace525e09d2f910
TristanDom <tortad1700@alumno.ipn.mx>
2022-11-07 09:29:10 -0600

    Segunda actualización del código conversor

--- ConversorCodigoBinario/Conversor.py
+++ ConversorCodigoBinario/Conversor.py
@@ -8,4 +8,15 @@ def ascii_a_binario(letra):
     # Convertirlo a binario
     return "{0:08b}".format(valor)
 
-print(ascii_a_binario("a"))
+def texto_a_binario(texto):
+    texto_binario = ""  # El resultado
+    contador = 0
+    for letra in texto:
+        texto_binario += ascii_a_binario(letra)
+        # Agregar un espacio entre binarios, excepto si es el último carácter
+        # if contador + 1 < len(texto):
+        #     texto_binario += separador
+        contador += 1
+    return texto_binario
+
+print(texto_a_binario("Hola mundo"))
