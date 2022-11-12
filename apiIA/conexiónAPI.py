c93b62e14f4995399f3094744da2f9a872f4270c
TristanDom <tortad1700@alumno.ipn.mx>
2022-11-11 19:19:46 -0600

    API con custom vision

--- apiIA/conexiónAPI.py
+++ apiIA/conexiónAPI.py
@@ -1,66 +1,37 @@
-from azure.cognitiveservices.vision.computervision import ComputerVisionClient
-from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
-from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
-from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
+from msrest.authentication import ApiKeyCredentials
+import os, time, uuid
 
-from array import array
-import os
-from PIL import Image
-import sys
-import time
+# Replace with valid values
+ENDPOINT = "https://southcentralus.api.cognitive.microsoft.com/"
+training_key = "c3f83fd12eef463cb0432303eca09f21"
+prediction_key = "69ca85c4f62940439a0e71c084ada721"
+prediction_resource_id = "/subscriptions/b4dbd004-9e91-48d4-9bbe-e70896ded8f6/resourceGroups/AUSB-AI/providers/Microsoft.CognitiveServices/accounts/AUSB-IA"
+publish_iteration_name = "ModeloFinal"
+project_id = "efd48c83-3ed8-4dde-a275-234866477704"
 
-'''
-Authenticate
-Authenticates your credentials and creates a client.
-'''
-subscription_key = "69ca85c4f62940439a0e71c084ada721"
-endpoint = "https://southcentralus.api.cognitive.microsoft.com/"
-
-computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
-'''
-END - Authenticate
-'''
-
-'''
-OCR: Read File using the Read API, extract text - remote
-This example will extract text in an image, then print results, line by line.
-This API call can also extract handwriting style text (not shown).
-'''
-print("===== Read File - remote =====")
-# Get an image with text
-read_image_url = "https://raw.githubusercontent.com/TristanDom/AUSB-AI/main/PaginasEscaneadas/PaginasConfiables/disney.png"
-
-# Call API with URL and raw response (allows you to get the operation location)
-read_response = computervision_client.read(read_image_url,  raw=True)
-
-# Get the operation location (URL with an ID at the end) from the response
-read_operation_location = read_response.headers["Operation-Location"]
-# Grab the ID from the URL
-operation_id = read_operation_location.split("/")[-1]
-
-print("===== Analyze an image - remote =====")
-# Select the visual feature(s) you want.
-remote_image_features = ["tags"]
-# remote_image_details = ["PaginasConfiables","PaginasDesconfiables"]
+credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
+trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
 
 
 
-# Call the "GET" API and wait for it to retrieve the results 
-while True:
-    read_result = computervision_client.get_read_result(operation_id)
-    if read_result.status not in ['notStarted', 'running']:
-        break
-    time.sleep(1)
+credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
+trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
 
-# Print the detected text, line by line
-if read_result.status == OperationStatusCodes.succeeded:
-    for text_result in read_result.analyze_result.read_results:
-        for line in text_result.lines:
-            print(line.text)
-            print(line.bounding_box)
-print()
-'''
-END - Read File - remote
-'''
 
-print("End of Computer Vision quickstart.")
+# Now there is a trained endpoint that can be used to make a prediction
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
+
+with open(os.path.join ("/home/hunter/Documentos/AUSB-AI/PaginasEscaneadas/", "test.png"), "rb") as image_contents:
+    results = predictor.classify_image(
+        project_id, publish_iteration_name, image_contents.read())
+
+    # Display the results.
+    for prediction in results.predictions:
+        print("\t" + prediction.tag_name +
+              ": {0:.2f}%".format(prediction.probability * 100))
